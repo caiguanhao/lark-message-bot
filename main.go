f55d52da79ab57dcd04f79148ed32507ffdfd02d
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2020-11-26 21:54:08 +0800

    fix word

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	appId := flag.String("app-id", "", "lark app id (you can also use env LARK_APP_ID)")
-	appSecret := flag.String("app-secret", "", "lark app id (you can also use env LARK_APP_SECRET)")
+	appSecret := flag.String("app-secret", "", "lark app secret (you can also use env LARK_APP_SECRET)")
 	chatIdFile := flag.String("chat-id", "chat.id", "file to store chat id")
 	address := flag.String("listen", "127.0.0.1:32123", "private http server address")
 	verbosity := flag.String("verbosity", "info", "debug, info, notice, warning, error, critical")
